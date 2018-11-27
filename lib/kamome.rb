3839e9f87170ac96f7c65a6492adb992ded46da1
kengos <kengo@kengos.jp>
2018-11-27 21:50:45 +0900

    initial commit

---
+++ lib/kamome.rb
@@ -0,0 +1,6 @@
+require "kamome/version"
+
+module Kamome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/kamome_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Kamome do
+  it "has a version number" do
+    expect(Kamome::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
